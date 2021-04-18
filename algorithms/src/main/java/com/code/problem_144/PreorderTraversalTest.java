82841071a6bdb1c44a75aee104c9446c31930969
huhongtao <huhongtao2015@163.com>
2021-04-18 17:28:19 +0800

    some algorithm problems

---
+++ algorithms/src/main/java/com/code/problem_144/PreorderTraversalTest.java
@@ -0,0 +1,23 @@
+package com.code.problem_144;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PreorderTraversalTest {
+
+    @Test
+    public void test() {
+        TreeNode tree = new TreeNode(1);
+        tree.left = new TreeNode(2);
+        tree.right = new TreeNode(3);
+        PreorderTraversal solution = new PreorderTraversal();
+        List<Integer> result = solution.preorderTraversal(tree);
+        List<Integer> expect = Arrays.asList(1, 2, 3);
+        Assert.assertEquals(result, expect);
+        result = solution.preorderTraversalV2(tree);
+        Assert.assertEquals(result, expect);
+    }
+}
---
+++ algorithms/src/main/java/com/code/problem_145/PostorderTraversalTest.java
@@ -0,0 +1,26 @@
+package com.code.problem_145;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PostorderTraversalTest {
+
+    @Test
+    public void test() {
+        TreeNode tree = new TreeNode(1);
+        tree.left = new TreeNode(2);
+        tree.right = new TreeNode(3);
+        PostorderTraversal solution = new PostorderTraversal();
+        List<Integer> expect = Arrays.asList(2, 3, 1);
+        List<Integer> result = solution.postorderTraversal(tree);
+        Assert.assertEquals(expect, result);
+        result = solution.postorderTraversalV2(tree);
+        Assert.assertEquals(expect, result);
+        result = solution.postorderTraversalV3(tree);
+        Assert.assertEquals(expect, result);
+    }
+
+}
